d8b905dcf58727379fff941bb1f0afe583d982e4
ronnielopez <39448096+ronnielopez@users.noreply.github.com>
2022-07-14 00:35:09 -0600

    Update homeModal.js

--- src/app/components/Home/homeModal.js
+++ src/app/components/Home/homeModal.js
@@ -6,22 +6,22 @@ import { getAudio, setPalabraDB, getImagen } from "../../services/palabras";
 import { PlayIcon, PauseIcon, TrashIcon } from '@heroicons/react/solid'
 
 const HomeModal = ({ palabra, handleClose, show }) => {
-    const [palabranew , setPalabra] = useState("")
+    const [palabranew, setPalabra] = useState("")
     const [significado, setSignificado] = useState("")
-    const [traduccion , setTraduccion] = useState("")
+    const [traduccion, setTraduccion] = useState("")
     const [imagen, setImagen] = useState(null)
     const [audio, setAudio] = useState(null)
-    const [playing , setPlaying] = useState(false);
+    const [playing, setPlaying] = useState(false);
 
-    useEffect(()=>{
-        if(palabra !== null){
+    useEffect(() => {
+        if (palabra !== null) {
             setPalabra(palabra.palabra)
             setSignificado(palabra.significado)
             setTraduccion(palabra.traduccion)
-            if (palabra.audio) getAudio(palabra.audio).then((url)=>(setAudio(url)))
-            if (palabra.imagen) getImagen(palabra.imagen).then((url)=>(setImagen(url)))
+            if (palabra.audio) getAudio(palabra.audio).then((url) => (setAudio(url)))
+            if (palabra.imagen) getImagen(palabra.imagen).then((url) => (setImagen(url)))
         }
-        else{
+        else {
             setAudio(null);
             setImagen(null);
             setPalabra('');
@@ -30,11 +30,11 @@ const HomeModal = ({ palabra, handleClose, show }) => {
         }
     }, [palabra])
 
-    const play = (playing)=>{
+    const play = (playing) => {
         const newAudio = new Audio(audio);
-        if(playing){
+        if (playing) {
             newAudio.play();
-        }else{
+        } else {
             newAudio.pause();
         }
     }
@@ -48,36 +48,37 @@ const HomeModal = ({ palabra, handleClose, show }) => {
                 <div className="form-group">
                     <label htmlFor="palabra" className="fw-bold">Palabra</label>
                     <input type="text" onChange={(ev) => { setPalabra(ev.target.value) }} value={palabranew} className="form-control" id="palabra" aria-describedby="emailHelp" placeholder="Ingresa la palabra" />
-                        <small id="emailHelp" className="form-text text-muted">Palabra para agregar al diccionario.</small>
+                    <small id="emailHelp" className="form-text text-muted">Palabra para agregar al diccionario.</small>
                 </div>
                 <div className="form-group my-3">
                     <label htmlFor="significado" className="fw-bold">Traduccion</label>
                     <input type="text" onChange={(ev) => { setTraduccion(ev.target.value) }} value={traduccion} className="form-control" id="traduccion" aria-describedby="emailHelp" placeholder="Ingresa la traduccion" />
-                        <small id="emailHelp" className="form-text text-muted">Traduccion a agregar al diccionario.</small>
+                    <small id="emailHelp" className="form-text text-muted">Traduccion a agregar al diccionario.</small>
                 </div>
                 <div className="form-group my-3">
                     <label htmlFor="significado" className="fw-bold">Significado</label>
                     <input type="text" onChange={(ev) => { setSignificado(ev.target.value) }} value={significado} className="form-control" id="significado" aria-describedby="emailHelp" placeholder="Ingresa el significado" />
-                        <small id="emailHelp" className="form-text text-muted">Significado a agregar al diccionario.</small>
+                    <small id="emailHelp" className="form-text text-muted">Significado a agregar al diccionario.</small>
                 </div>
-                {imagen === null ? <div className="form-group my-3">
+                {imagen !== null && palabra !== null ? <div className="text-center"> <img src={imagen} className="w-50" /> <button className="btn btn-danger"><TrashIcon className="icons" /></button> </div> : <div className="form-group my-3">
                     <label htmlFor="significado" className="fw-bold">Imagen</label>
                     <input type="file" onChange={(ev) => { setImagen(ev.target.files[0]) }} className="form-control" id="img" />
-                        <small id="emailHelp" className="form-text text-muted">Imagen de la escritura de la palabra.</small>
-                </div> : <div className="text-center"> <img src={imagen} className="w-50" /> <button className="btn btn-danger"><TrashIcon className="icons"/></button> </div>}
-                {audio === null ? <div className="form-group my-3">
-                    <label htmlFor="significado" className="fw-bold">Audio</label>
-                    <input type="file" onChange={(ev) => { setAudio(ev.target.files[0]) }} className="form-control" id="audio" />
+                    <small id="emailHelp" className="form-text text-muted">Imagen de la escritura de la palabra.</small>
+                </div>}
+                {audio !== null && palabra !== null ? <div className="my-4 text-center"> <h2>Audio</h2> <button className="btn btn-success" onClick={() => { setPlaying(!playing); play(playing) }}> {!playing ? <PlayIcon className="icons" /> : <PauseIcon className="icons" />} </button> </div> :
+                    <div className="form-group my-3">
+                        <label htmlFor="significado" className="fw-bold">Audio</label>
+                        <input type="file" onChange={(ev) => { setAudio(ev.target.files[0]) }} className="form-control" id="audio" />
                         <small id="emailHelp" className="form-text text-muted">Audio de la palabra.</small>
-                </div> : <div className="my-4 text-center"> <h2>Audio</h2> <button className="btn btn-success" onClick={()=>{setPlaying(!playing); play(playing)}}> {!playing ? <PlayIcon className="icons"/> : <PauseIcon className="icons"/> } </button> </div>}
-                
+                    </div>}
+
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" onClick={handleClose}>
                     Cerrar
                 </Button>
-                <Button variant="primary" onClick={()=>{
-                   if(palabra === null) setPalabraDB(palabranew, significado, traduccion,imagen, audio)
+                <Button variant="primary" onClick={() => {
+                    if (palabra === null) setPalabraDB(palabranew, significado, traduccion, imagen, audio)
                     handleClose()
                 }}>
                     {palabra !== null ? 'Modificar' : 'Agregar'}
